df2d7d41c6cddad16e78a387237c8fb41f7574de
876pol <paulchenrox3@gmail.com>
2022-01-18 22:03:56 -0500

    add files via upload

---
+++ Graphs and Trees/find_cycle_undirected.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+vector<vector<ll>> graph;
+vector<bool> visited;
+vector<ll> parent, cycle;
+ll cycle_start, cycle_end;
+
+bool dfs(ll curr, ll prev) {
+    visited[curr] = true;
+    for (ll u : graph[curr]) {
+        if (u == prev) continue;
+        if (visited[u]) {
+            cycle_end = curr;
+            cycle_start = u;
+            return true;
+        }
+        parent[u] = curr;
+        if (dfs(u, parent[u])) return true;
+    }
+    return false;
+}
+
+void find_cycle() {
+    ll n = graph.size();
+    visited.assign(n, false);
+    parent.assign(n, -1);
+    cycle_start = -1;
+    for (ll v = 0; v < n; v++) {
+        if (!visited[v] && dfs(v, parent[v])) break;
+    }
+    if (cycle_start != -1) {
+        cycle.push_back(cycle_start);
+        for (ll v = cycle_end; v != cycle_start; v = parent[v])
+            cycle.push_back(v);
+        cycle.push_back(cycle_start);
+        reverse(cycle.begin(), cycle.end());
+    }
+}
