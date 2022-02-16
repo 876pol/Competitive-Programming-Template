bfc1cddb9ebfe018e71caa205302be61c7cbffec
876pol <paulchenrox3@gmail.com>
2022-02-15 22:14:37 -0500

    add files via upload

---
+++ Dynamic Programming/knapsack01.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll n, w;
+vector<ll> dp;
+vector<pair<ll, ll>> a;  // weight, value
+
+void knapsack() {
+    dp.resize(w + 1);
+    for (ll i = 1; i <= n; i++) {
+        for (ll j = w; j >= 0; j--) {
+            if (j - a[i - 1].first >= 0) {
+                dp[j] = max(dp[j], dp[j - a[i - 1].first] + a[i - 1].second);
+            }
+        }
+    }
+}
