54b3fcfb36c7d4c03b5511d02c329a8bd69b8931
Paul Chen <chenpaul.pc@gmail.com>
2022-08-06 20:28:44 -0600

    update centroid_decomposition.cpp

--- graphs/centroid_decomposition.cpp
+++ graphs/centroid_decomposition.cpp
@@ -1,33 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 using ll = long long;
 
-ll n;
-vector<vector<ll>> graph;
-vector<ll> subtree_size;
+struct centroid_decomposition {
+    ll size;
+    vector<set<ll>> graph;
+    vector<ll> parent;
+    vector<ll> sub_size;
 
-ll get_subtree_size(ll curr, ll prev) {
-    subtree_size[curr] = 1;
-    for (ll i : graph[curr]) {
-        if (i == prev) continue;
-        subtree_size[curr] += get_subtree_size(i, curr);
+    centroid_decomposition(vector<vector<ll>> &graph) {
+        size = graph.size();
+        this->graph.assign(size, set<ll>());
+        parent.resize(size, 0);
+        sub_size.resize(size, 0);
+        for (ll i = 0; i < size; i++) {
+            for (auto to : graph[i]) {
+                this->graph[i].insert(to);
+                this->graph[to].insert(i);
+            }
+        }
+        build(1, -1);
     }
-    return subtree_size[curr];
-}
 
-ll get_centroid(ll curr, ll prev) {
-    for (ll i : graph[curr]) {
-        if (i == prev) continue;
-        if (subtree_size[i] * 2 > n) {
-            return get_centroid(i, curr);
+    void build(ll curr, ll prev) {
+        get_sub_size(curr, prev);
+        ll centroid = get_centroid(curr, prev, sub_size[curr]);
+        parent[centroid] = prev;
+        auto it = graph[centroid].begin();
+        while (it != graph[centroid].end()) {
+            ll sub = *it;
+            graph[sub].erase(centroid);
+            it = graph[centroid].erase(it);
+            build(sub, centroid);
         }
     }
-    return curr;
-}
 
-ll centroid_decomposition(ll root) {
-    subtree_size.resize(graph.size());
-    get_subtree_size(root, -1);
-    return get_centroid(root, -1);
-}
+    void get_sub_size(ll curr, ll prev) {
+        sub_size[curr] = 1;
+        for (auto sub : graph[curr]) {
+            if (prev == sub) continue;
+            get_sub_size(sub, curr);
+            sub_size[curr] += sub_size[sub];
+        }
+    }
+
+    ll get_centroid(ll curr, ll prev, ll tree_size) {
+        for (auto sub : graph[curr]) {
+            if (sub == prev || sub_size[sub] <= tree_size / 2) continue;
+            return get_centroid(sub, curr, tree_size);
+        }
+        return curr;
+    }
+};
