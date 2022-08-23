413c6e81666278f7cde510a6c2ceabe8a965b769
Paul Chen <paulchenrox3@gmail.com>
2022-08-22 21:26:28 -0400

    update files

--- math/ncr_query.cpp
+++ math/ncr_query.cpp
@@ -3,40 +3,42 @@ using namespace std;
 
 using ll = long long;
 
-#define MOD 1000000007
-
-vector<ll> fact;
-
-void preprocess(ll n) {
-	fact.assign(n + 1, 1);
-	for (ll i = 1; i <= n; i++) {
-		fact[i] = fact[i - 1] * i;
-		fact[i] %= MOD;
-	}
-}
-
-ll gcd(ll a, ll b) { 
-    return ((b == 0) ? a : gcd(b, a % b)); 
-}
-
-ll mod_inverse(ll a) {
-    if (gcd(a, MOD) != 1) return -1;
-    ll m0 = MOD;
-    ll m = MOD;
-    ll y = 0, x = 1;
-    if (m == 1) return 0;
-    while (a > 1) {
-        ll q = a / m;
-        ll t = m;
-        m = a % m, a = t;
-        t = y;
-        y = x - q * y;
-        x = t;
+ll extended_euclidean(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
     }
-    if (x < 0) x += m0;
-    return x;
+    ll x1, y1;
+    ll d = extended_euclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
 }
 
-ll ncr(ll n, ll r) {
-	return fact[n] * mod_inverse(fact[n - r] * fact[r] % MOD) % MOD;
+ll mod_inverse(ll a, ll m) {
+    ll x, y;
+    if (extended_euclidean(a, m, x, y) != 1) {
+        return -1;
+    }
+    return (x % m + m) % m;
 }
+
+struct ncr {
+    vector<ll> fact;
+
+    ncr() {
+    }
+
+    ncr(ll n) {
+        fact.assign(n + 1, 1);
+        for (ll i = 1; i <= n; i++) {
+            fact[i] = fact[i - 1] * i;
+            fact[i] %= MOD;
+        }
+    }
+
+    ll _ncr(ll n, ll r) {
+        return fact[n] * mod_inverse(fact[n - r] * fact[r] % MOD, MOD) % MOD;
+    }
+};
--- strings/polynomial_hash.cpp
+++ strings/polynomial_hash.cpp
@@ -3,14 +3,65 @@ using namespace std;
 
 using ll = long long;
 
-ll compute_hash(string const& s) {
-    const ll p = 31;
-    const ll m = 1e9 + 7;
-    ll hash_value = 0;
-    ll p_pow = 1;
-    for (char c : s) {
-        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
-        p_pow = (p_pow * p) % m;
+ll extended_euclidean(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
     }
-    return hash_value;
+    ll x1, y1;
+    ll d = extended_euclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
 }
+
+ll mod_inverse(ll a, ll m) {
+    ll x, y;
+    if (extended_euclidean(a, m, x, y) != 1) {
+        return -1;
+    }
+    return (x % m + m) % m;
+}
+
+struct ncr {
+    vector<ll> fact;
+
+    ncr() {
+    }
+
+    ncr(ll n) {
+        fact.assign(n + 1, 1);
+        for (ll i = 1; i <= n; i++) {
+            fact[i] = fact[i - 1] * i;
+            fact[i] %= MOD;
+        }
+    }
+
+    ll _ncr(ll n, ll r) {
+        return fact[n] * mod_inverse(fact[n - r] * fact[r] % MOD, MOD) % MOD;
+    }
+};
+
+struct polynomial_hash {
+    ll size, P = 31;
+    vector<ll> p_pow, hash_value;
+    ncr _ncr;
+
+    polynomial_hash(const string& s) {
+        size = s.size();
+        p_pow.assign(size + 1, 0);
+        p_pow[0] = 1;
+        hash_value.assign(size + 1, 0);
+        _ncr = ncr(size + 1);
+        for (ll i = 0; i < size; i++) {
+            hash_value[i + 1] =
+                (hash_value[i] + (s[i] - 'a' + 1) * p_pow[i]) % MOD;
+            p_pow[i + 1] = (p_pow[i] * P) % MOD;
+        }
+    }
+
+    ll get_hash(ll l, ll r) {
+        return (hash_value[r] - hash_value[l] + MOD) * mod_inverse(p_pow[l + 1], MOD) % MOD;
+    }
+};
